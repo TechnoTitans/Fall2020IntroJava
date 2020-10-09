6e6af0b2581900cc13261f1dc2d42cfcc1e4806d
Berkan Ottlik <berkott3002@gmail.com>
2020-10-09 16:28:22 -0400

    oops

--- src/connectFour.java
+++ src/connectFour.java
@@ -35,5 +35,28 @@ public class connectFour {
 //      Technical details
 //        - 2d Array to store game state
 //        - while loop
+
+        String[][] board = {
+                {" ", " ", " ", " ", " ", " ", " "},
+                {" ", " ", " ", " ", " ", " ", " "},
+                {" ", " ", " ", " ", " ", " ", " "},
+                {" ", " ", " ", " ", " ", " ", " "},
+                {" ", " ", " ", " ", " ", " ", " "},
+                {" ", " ", " ", " ", " ", " ", " "}
+        };
+
+        printBoard(board);
+    }
+
+    private static void printBoard (String[][] board) {
+        System.out.println("+-------------+");
+        for (int i = 0; i < board.length; i++) {
+            if (i != 0) {
+                System.out.println("+-+-+-+-+-+-+-+");
+            }
+            System.out.println("|" + board[i][0] + "|" + board[i][1] + "|" + board[i][2] + "|"
+                    + board[i][3] + "|" + board[i][4] + "|" + board[i][5] + "|" + board[i][6] + "|");
+        }
+        System.out.println("+-------------+");
     }
 }
